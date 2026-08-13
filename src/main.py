3eacfd452756ca04057ab0ff4547fa3f9277357f
Patricia Souza <patricia.sz.br@gmail.com>
2026-08-13 20:46:49 -0300

    Query Parameter

--- src/main.py
+++ src/main.py
@@ -28,7 +28,15 @@ def inicio():
 @app.get("/jogador/{id_jogador}")
 def get_jogador(id_jogador: int):
    return jogadores[id_jogador]
-   
+
+
+@app.get("/jogador-time")
+def get_jogador_time(time: str):
+  for jogador_id in jogadores:
+    if jogadores[jogador_id]["time"] == time:
+        return jogadores[jogador_id]
+  return {"Dados": "Nao foi encontrado"}
+         
 
 def main():
     print("Hello from fundamentos-fastapi!")
