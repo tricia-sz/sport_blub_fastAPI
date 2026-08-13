a58bf763c485b08e43a6b34c5aa9e3ba9ddfe68e
Patricia Souza <patricia.sz.br@gmail.com>
2026-08-13 20:36:32 -0300

    Path Parameter

--- src/main.py
+++ src/main.py
@@ -2,10 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+jogadores = {
+   1: {
+      "nome": "Yuri Alberto",
+      "idade": 23,
+      "time": "Corinthians"
+   },
+   2: {
+      "nome": "Rodrigo Garro",
+      "idade": 28,
+      "time": "Corinthians"
+    },
+    3: {
+      "nome": "Hugo Souza",
+      "idade": 25,
+      "time": "Corinthians"
+    },
+    
+}
+
 @app.get("/")
 def inicio():
-  return{"Mensagem": "Olá Mundo"}
+  return jogadores
 
+@app.get("/jogador/{id_jogador}")
+def get_jogador(id_jogador: int):
+   return jogadores[id_jogador]
+   
 
 def main():
     print("Hello from fundamentos-fastapi!")
