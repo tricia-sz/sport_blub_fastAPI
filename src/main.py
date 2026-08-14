6f2376bdc2956b3aeb75804f34a62556c7e0b57d
Patricia Souza <patricia.sz.br@gmail.com>
2026-08-13 23:43:55 -0300

    iniciando FAMR com React

--- src/main.py
+++ src/main.py
@@ -4,12 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def inicio():
-  return{"Mensagem": "Olá Mundo"}
+  return{"Mensagem": "Iniciando FARM STACK"}
 
 
-def main():
-    print("Hello from fundamentos-fastapi!")
 
-
-if __name__ == "__main__":
-    main()
