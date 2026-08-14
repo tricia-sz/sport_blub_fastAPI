da215a994fb27a6070be44abe753139299ad1618
Patricia Souza <patricia.sz.br@gmail.com>
2026-08-14 14:02:23 -0300

    Inserindo jogadores no banco

--- src/routes/jogador.py
+++ src/routes/jogador.py
@@ -2,13 +2,21 @@ from fastapi import APIRouter
 
 from config.database import conexao
 from models.jogador import Jogador
+from schemas.jogador import jogadorEntidade, listaJogadoresEntidade
 
 jogador_router =  APIRouter()
 
-@jogador_router.get("/")
+@jogador_router.get('/')
 async def inicio():
   return "Bem-vindo ao FullStack Farm"
 
-@jogador_router.get("/jogadores")
+@jogador_router.get('/jogadores')
 async def lista_jogadores():
-  return conexao.local.jogador.find()
+  return listaJogadoresEntidade(conexao.local.jogador.find())
+
+# Insere novos jogadoresmongodb://localhost:27017/
+@jogador_router.post('/jogadores')
+async def  cadastra_jogadores(jogador: Jogador):
+  conexao.local.jogador.insert_one(dict(jogador))
+  return listaJogadoresEntidade(conexao.local.jogador.find())
+
