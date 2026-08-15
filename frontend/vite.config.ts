9c669f89c0b256800081c1178e30fba8f96dd355
Patricia Souza <patricia.sz.br@gmail.com>
2026-08-15 15:19:04 -0300

    seprando modulos

--- frontend/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Importe aqui
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()], // Adicione aqui
-})
